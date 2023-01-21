81f71ca7223e05a00d82857f7a836c3682d01e2d
francomirko91 <francomirko91@gmail.com>
2023-01-21 14:06:49 +0100

    ----------

---
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+
+        setContentView(view)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,firstFragment)
+            commit()
+        }
+
+        binding.btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment,firstFragment)
+                commit()
+            }
+
+        }
+
+        binding.btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment,secondFragment)
+                commit()
+            }
+
+        }
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/fragment/btn.kt
@@ -0,0 +1,5 @@
+package com.example.fragment
+
+object btn {
+
+}
